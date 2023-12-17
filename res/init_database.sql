f90bb6a3a43b1adea41473a3191afd45b144c045
Lahène Belhadi <lahcene.belhadi@gmail.com>
2023-12-17 17:42:04 +0100

    fix: Adds postgresql configuration & init script

---
+++ res/init_database.sql
@@ -0,0 +1,22 @@
+-- This script contains all the initialization procedures for the Player  
+-- Collection database
+--
+-- To appy changes to an existing database, just run this script once again, it 
+-- will execute all the ALTER procedures it contains without whipping off the 
+-- existing data.
+--
+-- Authors: Lahcène Belhadi <lahcene.belhadi@gmail.com>
+ 
+ -- Creates the Player collection database and its tables
+ -- # Tables:
+ -- * character_unique
+CREATE DATABASE playercollectiondb;
+
+-- Go to player collection
+\c playercollectiondb
+
+CREATE TABLE IF NOT EXISTS character (
+	id BIGSERIAL PRIMARY KEY,
+	player BIGINT NOT NULL,
+	character BIGINT NOT NULL
+);
